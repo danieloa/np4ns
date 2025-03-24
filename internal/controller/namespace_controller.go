4a1bf6d56393b269aa55a9ce9035517c339390fd
Daniel Ortega <dortega@cloudbees.com>
2025-03-24 10:49:05 +0100

    catch op and react based on it

--- internal/controller/namespace_controller.go
+++ internal/controller/namespace_controller.go
@@ -66,6 +66,25 @@ func (r *NamespaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	op, err := ctrl.CreateOrUpdate(ctx, r.Client, ns, func() error {
 		logger.Info("CreateOrUpdate", "ns", ns.Name)
 
+		// because we are using CreateOrUpdate, we need to modify the namespace object to trigger the create/update operation, else we will get a no-op "unchanged"
+		// + sets the label on the namespace
+		if ns.Labels == nil {
+			ns.Labels = make(map[string]string)
+		}
+		ns.Labels["network-policy"] = "managed"
+		return nil
+	})
+
+	if err != nil {
+		logger.Error(err, "unable to create or update Namespace")
+		return ctrl.Result{}, err
+	}
+	logger.Info("operation:", "op", op)
+	switch op {
+	case controllerutil.OperationResultCreated:
+		// A new namespace is created
+		logger.Info("Namespace being created -> network policy enforced", "namespace", ns)
+
 		// lets see if the network policy already exists
 		npr := &networkingv1.NetworkPolicy{}
 
@@ -92,37 +111,69 @@ func (r *NamespaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 				}
 				if np_err = ctrl.SetControllerReference(ns, npc, r.Scheme); np_err != nil {
 					logger.Error(np_err, "unable to set owner reference on NetworkPolicy") // this is not needed, the error is already logged by the system
-					return np_err
+					return ctrl.Result{}, np_err
 				}
 
 				if np_err = r.Create(ctx, npc); np_err != nil {
 					logger.Error(np_err, "unable to create NetworkPolicy") // this is not needed, the error is already logged by the system
-					return np_err
+					return ctrl.Result{}, np_err
 				}
-
 			}
 		}
-		// because we are using CreateOrUpdate, we need to modify the namespace object to trigger the create/update operation, else we will get a no-op "unchanged"
-		// + sets the label on the namespace
-		if ns.Labels == nil {
-			ns.Labels = make(map[string]string)
-		}
-		ns.Labels["network-policy"] = "enforced"
-		return nil
-	})
-
-	if err != nil {
-		logger.Error(err, "unable to create or update Namespace")
-		return ctrl.Result{}, err
-	}
-	logger.Info("operation:", "op", op)
-	switch op {
-	case controllerutil.OperationResultCreated:
-		// A new namespace is created
-		logger.Info("Namespace being created -> network policy enforced", "namespace", ns)
 	case controllerutil.OperationResultUpdated:
 		// a namespace is updated
 		logger.Info("Namespace being updated -> network policy enforced", "namespace", ns)
+		// lets see if the network policy already exists
+		npr := &networkingv1.NetworkPolicy{}
+
+		np_err := r.Get(ctx, client.ObjectKey{
+			Namespace: ns.Name,
+			Name:      "enforced-network-policy",
+		}, npr)
+
+		if np_err != nil {
+			if errors.IsNotFound(np_err) {
+				// there is no network policy, set controller reference and create it
+				npc := &networkingv1.NetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "enforced-network-policy",
+						Namespace: ns.Name,
+					},
+					Spec: networkingv1.NetworkPolicySpec{
+						PodSelector: metav1.LabelSelector{},
+						PolicyTypes: []networkingv1.PolicyType{
+							networkingv1.PolicyTypeEgress,
+						},
+						Egress: []networkingv1.NetworkPolicyEgressRule{},
+					},
+				}
+				if np_err = ctrl.SetControllerReference(ns, npc, r.Scheme); np_err != nil {
+					logger.Error(np_err, "unable to set owner reference on NetworkPolicy") // this is not needed, the error is already logged by the system
+					return ctrl.Result{}, np_err
+				}
+
+				if np_err = r.Create(ctx, npc); np_err != nil {
+					logger.Error(np_err, "unable to create NetworkPolicy") // this is not needed, the error is already logged by the system
+					return ctrl.Result{}, np_err
+				}
+			}
+		} else {
+			if np_err = ctrl.SetControllerReference(ns, npr, r.Scheme); np_err != nil {
+				// sets the owner reference on the network policy
+				logger.Error(np_err, "unable to set owner reference on NetworkPolicy") // this is not needed, the error is already logged by the system
+				return ctrl.Result{}, np_err
+			}
+			if np_err = r.Delete(ctx, npr); np_err != nil {
+				// assumes np is wrong and deletes the existing one
+				logger.Error(np_err, "unable to delete NetworkPolicy") // this is not needed, the error is already logged by the system
+				return ctrl.Result{}, np_err
+			}
+			if np_err = r.Create(ctx, npr); np_err != nil {
+				// creates a new network policy
+				logger.Error(np_err, "unable to create NetworkPolicy") // this is not needed, the error is already logged by the system
+				return ctrl.Result{}, np_err
+			}
+		}
 	}
 	return ctrl.Result{}, nil
 }
@@ -137,3 +188,9 @@ func (r *NamespaceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Owns(&networkingv1.NetworkPolicy{}).
 		Complete(r)
 }
+
+// TODO:
+// + CreateOrUpdate should only modify the ns.labels and return either `nil` or `ctrl.SetControllerReference(ns, npc, r.Scheme)`
+// + as the logic to create the network policy shoud be gone bc ^^^. an implementation of it should go inside the respective case statement
+// + the logger.Error should be removed from the CreateOrUpdate function
+// + the logger.Info should be removed from the CreateOrUpdate function
